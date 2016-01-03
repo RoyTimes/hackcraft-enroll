2535bf17aaf1abb68fc2391a63daed3913b26006
RoyTimes <roytimes@icloud.com>
2016-01-03 22:56:16 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var fs = require ('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/enroll', function (req, res) {
+
+	var getName = req.body.name;
+	var getEmail = req.body.email;
+	var getQQ = req.body.qq;
+	var getGender = req.body.gender;
+	var getPhone = req.body.phone;
+
+	console.log("name: " + getName + " Email: " + getEmail + " QQ: "
+				+ getQQ + " Phone: " + getPhone + " Gender: " + getGender);
+
+	var data = {
+		name: getName,
+		email: getEmail,
+		qq: getQQ,
+		phone: getPhone,
+		gender: getGender
+	};
+	console.log(data);
+
+	fs.appendFile('namelist.json', data, function(err){
+		if (err) console.log(err);
+		else console.log("Data saved");
+	});
+});
+
+
+module.exports = router;
